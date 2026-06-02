a763fd466b3071bf8a30764b15749526f0480730
CCaroline28 <145038653+CCaroline28@users.noreply.github.com>
2026-06-02 13:50:09 +0200

    e

--- scripts/F3_carte.R
+++ scripts/F3_carte.R
@@ -1,17 +1,25 @@
 c <- read.csv("C:/Users/carol/Documents/Cours/Projet/voiture_electrique/Big-Data-Projet/data/fichier_nettoye.csv",header = TRUE, sep = ',');
 
-c["code_insee_commune"];
+c["coordonneesXY"];
 
-library(mapsf)
-library(sf)
-mtq <- mf_get_mtq()
-mf_map(x = mtq, var = "FR", type = "choro")
+library(mapsf) #sert à afficher la carte
+library(sf) #pour tout ce qui est relatif à l'affiche de carte
 
-install.packages(c("units", "classInt"))
-install.packages("sf")
-library(sf)
-dep <- st_read("../data/lot46.gpkg", layer = "departement", quiet = TRUE)
-mf_map(x = mtq, var = dep, type = "choro")
-mf_map(x = dep, lwd = 2, col = NA, add = TRUE) # affiche le contour des départements
-sf::st_read
-getwd()
+
+dep <- st_read("../data/lot46.gpkg", layer = "departement", quiet = TRUE) #Lit le fichier qui contient les données de la carte
+
+mf_map(x = dep) # affiche la carte de la france
+
+
+st_crs(dep) #permet de connaitre la norme des coordonnées de la carte (ici ; EPSG:2154 — Lambert‑93)
+
+separate(c["coordonneesXY"], into = c("X", "Y"), sep = ", ", convert = TRUE)
+
+for (coord in c["coordonneesXY"]) {
+  print(coord[,1])
+  #point_sf <- st_as_sf(c, coords = c(coord), crs = 2154)  # convertit les coord en points sf
+  #points_dep <- st_join(point_sf, dep) # jointure spacial -> ajoute une colonne avec le département correspondant 
+}
+
+points_dep
+
