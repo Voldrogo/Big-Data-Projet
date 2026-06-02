5813fc1a552b751c8554d9dd18b0f7b899ff3408
CCaroline28 <145038653+CCaroline28@users.noreply.github.com>
2026-06-02 09:30:20 +0200

    Update F3_carte.R

--- scripts/F3_carte.R
+++ scripts/F3_carte.R
@@ -1,2 +1,11 @@
-c <- read.table("C:\Users\carol\Documents\Cours\Projet\IRVE.csv",header = TRUE);
+c <- read.csv("C:/Users/carol/Documents/Cours/Projet/voiture_electrique/Big-Data-Projet/data/fichier_nettoye.csv",header = TRUE, sep = ',');
 
+c["coordonneesXY"];
+
+library(mapsf)
+mtq <- mf_get_mtq()
+mf_map(x = mtq, var = "FR", type = "choro")
+
+dep <- st_read("data/lot46.gpkg", layer = "departement", quiet = TRUE)
+mf_map(x = mtq, var = dep, type = "choro")
+mf_map(x = dep, lwd = 2, col = NA, add = TRUE) # affiche le contour des départements
